632eabb26a8e28d4c8ce0a20532d9bc9993cecb5
Davide Cenzi <link82@gmail.com>
2015-07-30 00:19:00 +0200

    inhibited sections, created section_page scope

---
+++ app/models/concerns/section_page.rb
@@ -0,0 +1,43 @@
+module SectionPage
+  extend ActiveSupport::Concern
+
+  included do
+
+    class << self
+      def extra_fields_config
+        super.merge({
+          section_landing_page: :boolean
+        })
+      end
+
+      def visible_field
+        super - [:section_id]
+      end
+    end
+
+
+    ## Landing logic
+    # Verify that only one page can be set ad landing page for a specific section
+    after_save :check_section
+    scope :landing_for_section, ->{where("cms_contents.extra_fields LIKE ?","%section_landing_page: '1'%")}
+
+    protected
+
+    def check_section
+      unless self.section
+        self.section_landing_page = false
+      else
+        if self.section_landing_page
+          #unset previous new opening records
+          k = self.class
+          shops = k.landing_for_section(k.name.to_s.underscore)
+          shops.each do |s|
+            s.section_landing_page = false
+            s.save
+          end
+        end
+      end
+    end
+
+  end
+end
