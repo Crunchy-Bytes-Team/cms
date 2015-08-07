5f5222da526c786c9bb28d7506ce52b5cc4f6a35
Davide Cenzi <link82@gmail.com>
2015-08-07 10:01:09 +0200

    removed senseless migrations from gem

--- db/migrate/20150329141213_remove_attributes_from_courses.rb
+++
@@ -1,7 +0,0 @@
-class RemoveAttributesFromCourses < ActiveRecord::Migration
-  def change
-    remove_column :courses, :timing_frequency
-    remove_column :courses, :price
-    remove_column :courses, :subscription
-  end
-end
